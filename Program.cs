18669eab49bced1fd896097cbc66277dc0834cec
ederribdias <ederribeirodias@gmail.com>
2021-02-03 17:50:55 -0300

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nComo você se chama?");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"\nOlá, {name}, hoje é dia {date:d} e são {date:t}!");
+            Console.Write("\nPressione qualquer tecla para sair...");
+            Console.ReadKey(true);
+        }
+    }
+}
